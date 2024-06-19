bed24df6e08ef530b139dd6913590c6776c8533f
Carlos May <carlosmg.may@icloud.com>
2024-06-18 23:12:05 -0400

    [ResultPattern]->{CREATE}

---
+++ ObjectFunctionalSandbox/Patterns/ResultPattern.cs
@@ -0,0 +1,47 @@
+
+namespace ObjectFunctionalSandbox.Patterns;
+
+public class Result<T>
+{
+    /// <summary>
+/// Value prop.
+/// </summary>
+/// <value></value>
+public T Value { get; set; }
+
+/// <summary>
+/// IsSuccess prop.
+/// </summary>
+/// <value></value>
+public bool IsSuccess { get; }
+
+/// <summary>
+/// Error prop.
+/// </summary>
+/// <value></value>
+public string? Error { get; }
+
+private Result(T value, bool isSuccess, string? error)
+{
+    Value = value;
+    IsSuccess = isSuccess;
+    Error = error;
+}
+
+public static Result<T> Success(T value) => new Result<T>( value, true, null);
+
+public static Result<T> Failure( string error) => new Result<T>( default, false, error);
+
+}
+
+public static class ResultPattern
+{
+    public static Result<int> Divide( int numerator, int denominator )
+    {
+        if( denominator == 0 )
+            return Result<int>.Failure( "Denominator must be higher than zero");
+
+        return Result<int>.Success( numerator / denominator );
+    }
+
+}
