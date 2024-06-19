51ee987ab07aaaa8588cd98860256f5c675477cf
Carlos May <carlosmg.may@icloud.com>
2024-06-18 23:19:14 -0400

    [ResultPattern] -> {UPDATE}: CALL ResultPattern.Divide().
    :triangular_flag_on_post:  Call Result pattern to test it.

--- ObjectFunctionalSandbox/Program.cs
+++ ObjectFunctionalSandbox/Program.cs
@@ -1,42 +1,8 @@
-﻿Result<int> Divide( int numerator, int denominator )
-{
-    if( denominator == 0 )
-        return Result<int>.Failure( "Denominator must be higher than zero");
+﻿using ObjectFunctionalSandbox.Patterns;
 
-    return Result<int>.Success( numerator / denominator );
-}
+var result = ResultPattern.Divide( 10,0 );
 
-public class Result<T>
-{
-    /// <summary>
-    /// Value prop.
-    /// </summary>
-    /// <value></value>
-    public T Value { get; set; }
-
-    /// <summary>
-    /// IsSuccess prop.
-    /// </summary>
-    /// <value></value>
-    public bool IsSuccess { get; }
-
-    /// <summary>
-    /// Error prop.
-    /// </summary>
-    /// <value></value>
-    public string? Error { get; }
-
-    private Result(T value, bool isSuccess, string? error)
-    {
-        Value = value;
-        IsSuccess = isSuccess;
-        Error = error;
-    }
-
-    public static Result<T> Success(T value) => new Result<T>( value, true, null);
-
-    public static Result<T> Failure( string error) => new Result<T>( default, false, error);
-
-
-}
-
+if( result.IsSuccess )
+    Console.WriteLine( result.Value );
+else
+    Console.WriteLine( result.Error );
